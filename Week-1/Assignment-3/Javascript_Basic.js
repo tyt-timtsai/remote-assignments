3aaf0dc94d3d5ea0b93a911f5ff238fcb2b12c5e
Tim Tsai <81073535+tyt-timtsai@users.noreply.github.com>
2022-06-11 01:18:44 +0800

    finish first thought of these 2 function.
    先用最直觀的方法寫。

--- Week-1/Assignment-3/Javascript_Basic.js
+++ Week-1/Assignment-3/Javascript_Basic.js
@@ -1,9 +1,42 @@
 function countAandB(input){
-
+    let arrayAandB = [];
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === 'a'|| input[i] === 'b') {
+            arrayAandB.push(input[i])
+            // console.log(input[i]);
+            // console.log(arrayAandB);
+        }
+    }
+    // console.log(arrayAandB);
+    return arrayAandB.length;
 }
 
 function toNumber(input){
-
+    /* 最直觀做法 a=1 b=2 ......  */
+    let arrayOfNumber = [];
+    for (let i = 0; i < input.length; i++) {
+        switch (input[i]) {
+            case 'a':
+                arrayOfNumber.push(1);
+                break;
+            case 'b':
+                arrayOfNumber.push(2);
+                break;
+            case 'c':
+                arrayOfNumber.push(3);
+                break;
+            case 'd':
+                arrayOfNumber.push(4);
+                break;
+            case 'e':
+                arrayOfNumber.push(5);
+                break;
+            default:
+                console.log(arrayOfNumber);
+                break;
+        }
+    }
+    return arrayOfNumber;
 }
 
 let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
