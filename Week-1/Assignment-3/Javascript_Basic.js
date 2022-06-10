4db4d4d09bc170690757ccd1c22ff4388c7f2714
TimTsai <theblazertim1020@gmail.com>
2022-06-11 00:04:47 +0800

    Setup for Assigment-3

---
+++ Week-1/Assignment-3/Javascript_Basic.js
@@ -0,0 +1,15 @@
+function countAandB(input){
+
+}
+
+function toNumber(input){
+
+}
+
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+console.log(countAandB(input1));
+console.log(toNumber(input1));
+
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2));
+console.log(toNumber(input2));
