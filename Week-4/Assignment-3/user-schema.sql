a0485cdc5347567b4989236775cf6bb1e92442e2
TimTsai <theblazertim1020@gmail.com>
2022-07-04 21:19:57 +0800

    Add Mysqldump

--- Week-4/Assignment-3/user-schema.sql
+++ Week-4/Assignment-3/user-schema.sql
@@ -1,18 +1,54 @@
-CREATE DATABASE assignment;
-USE `assignment`;
-CREATE TABLE `user`(
-	`id` INT NOT NULL UNIQUE AUTO_INCREMENT,
-    `email` VARCHAR(64) NOT NULL UNIQUE,
-    `password` VARCHAR(64) NOT NULL,
-    PRIMARY KEY(`id`)
-);
+-- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
+--
+-- Host: localhost    Database: assignment
+-- ------------------------------------------------------
+-- Server version	8.0.27
 
-DROP TABLE user;
--- 新增屬性
--- ALTER TABLE `user` ADD `address` VARCHAR(255)
--- 刪除屬性
--- ALTER TABLE `user` DROP COLUMN `address`
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO `user` VALUE(1, '123@gmail.com','123123');
--- INSERT INTO `user` (`email`, `password`) VALUE('456@gmail.com','000456');
-SELECT * FROM user;
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'123@gmail.com','123123'),(2,'456@gmail.com','456'),(3,'00000@gmail.com','00000'),(5,'000000@gmail.com','000000'),(6,'789@gmail.com','789'),(7,'asdqwe@gmail.com','asdqwe'),(8,'zxc123@gmail.com','zxc123'),(9,'qwert@gmail.com','qwert'),(10,'1233211@gmail.com','32123'),(11,'123qwe@gmail.com','123qwe'),(12,'123qwert@gmail.com','123qwert'),(13,'aaaaa@gmail.com','aaaaa'),(14,'bbbbb@gmail.com','bbbbb');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-04 21:17:42
